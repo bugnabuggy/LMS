175535c8492b5ba910df9292f4a052dbaa9aacc4
AnTony <jokerosky@gmail.com>
2016-02-20 17:05:12 +0600

    Created new project from VS pattern.

---
+++ src/LMS/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace LMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var shellPath = PlatformServices.Default.Application.ApplicationBasePath + @"\shell.html";
+            //var content = System.IO.File.ReadAllText(shellPath);
+
+            //return Content(content,"text/html");
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
